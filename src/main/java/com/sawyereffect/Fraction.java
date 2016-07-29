f3dd0f54fb70a2ba10a4f9aedd48d88f8d20d289
Javier Cervantes <javier@sawyereffect.com>
2016-07-29 17:01:50 -0400

    Adding gdc and refactoring
    Adding test case to have fraction expressed in lower terms

--- src/main/java/com/sawyereffect/Fraction.java
+++ src/main/java/com/sawyereffect/Fraction.java
@@ -2,25 +2,35 @@ package com.sawyereffect;
 
 public class Fraction {
 
-    public int numerator;
-    public int denominator;
-
+    private int numerator;
+    private int denominator;
 
     Fraction(int numerator, int denominator) {
         if (denominator == 0) {
             throw new IllegalArgumentException("Denominator can't be 0");
         }
-        this.numerator  = numerator;
+        this.numerator = numerator;
         this.denominator = denominator;
     }
 
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
     public Fraction add(Fraction addend) {
-        if (this.denominator == addend.denominator) {
-            int numeratorSum = this.numerator + addend.numerator;
-            return new Fraction(numeratorSum, this.denominator);
-        } else {
-            int numeratorSum = (this.numerator * addend.denominator) + (addend.numerator * this.denominator);
-            return new Fraction(numeratorSum, this.denominator * addend.denominator);
-        }
+        int denominatorSum = this.denominator * addend.denominator;
+        int numeratorSum = (this.numerator * addend.denominator) + (addend.numerator * this.denominator);
+
+        int gcd = gcd(denominatorSum, numeratorSum);
+        return new Fraction(numeratorSum / gcd, denominatorSum / gcd);
+    }
+
+    private static int gcd(int p, int q) {
+        if (q == 0) return p;
+        else return gcd(q, p % q);
     }
 }
--- src/test/java/com/sawyereffect/FractionTest.java
+++ src/test/java/com/sawyereffect/FractionTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 
 public class FractionTest {
 
-    @Test(expected=IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void invalidDenominator() {
         new Fraction(3, 0);
     }
@@ -15,23 +15,31 @@ public class FractionTest {
     public void sumWithSameDenominator() {
         Fraction result = new Fraction(7, 5).add(new Fraction(4, 5));
 
-        assertEquals(result.numerator, 11);
-        assertEquals(result.denominator, 5);
+        assertEquals(result.getNumerator(), 11);
+        assertEquals(result.getDenominator(), 5);
     }
 
     @Test
     public void sumWithDifferentDenominator() {
         Fraction result = new Fraction(7, 3).add(new Fraction(4, 5));
 
-        assertEquals(result.numerator, 47);
-        assertEquals(result.denominator, 15);
+        assertEquals(result.getNumerator(), 47);
+        assertEquals(result.getDenominator(), 15);
     }
 
     @Test
     public void sumIntegers() {
         Fraction result = new Fraction(2, 1).add(new Fraction(2, 1));
 
-        assertEquals(result.numerator, 4);
-        assertEquals(result.denominator, 1);
+        assertEquals(result.getNumerator(), 4);
+        assertEquals(result.getDenominator(), 1);
+    }
+
+    @Test
+    public void expressFractionsInLowestTerms() {
+        Fraction result = new Fraction(3, 5).add(new Fraction(2, 10));
+
+        assertEquals(result.getNumerator(), 4);
+        assertEquals(result.getDenominator(), 5);
     }
 }
